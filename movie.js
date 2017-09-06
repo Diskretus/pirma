9a90f3f8b39f8505f02468797f3a833b8e43c514
Diskretus <Diskretus22@gmail.com>
2017-09-06 12:35:47 +0300

    added movie.js

---
+++ movie.js
@@ -0,0 +1,14 @@
+var movies = [
+    {
+    name: 'Avatar',
+    actors: 'Sam Wortington, Zoe Saldana'
+    },
+    {
+    name: 'Titanic',
+    actors: 'Leonard DiCaprio, Kate Winslet'
+    },
+    {
+    name: 'Shrek',
+    actors: 'Mike Myers, Eddy Murphy'
+    }
+];
